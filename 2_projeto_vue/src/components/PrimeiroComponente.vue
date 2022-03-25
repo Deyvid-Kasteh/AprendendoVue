2859752f0dd7bec79879ec3eecdd80fa078a08a2
Deyvid Kasteh <99744584+Deyvid-Kasteh@users.noreply.github.com>
2022-03-25 18:31:56 -0300

    aula 06 componentes part 2

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
+    <div>
     <h1>Olá vue!</h1>
+    <p>Meu nome é {{ nome }} e minha função é {{profissao}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'PrimeiroComponente'
+    name: 'PrimeiroComponente',
+    data() {
+        return {
+            nome: "Deyvid",
+            profissao: "programador"
+        }
+    }
 }
 </script>
